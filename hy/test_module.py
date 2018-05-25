ce434dd8cfbeb46f6998a76c91f0950cf87994b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-25 10:43:43 +0200

    Test module imported and used from Hy

---
+++ hy/test_module.py
@@ -0,0 +1,13 @@
+THE_ANSWER = 42
+
+
+def multiply_two_numbers(x, y):
+    return x * y
+
+
+class uber_class:
+    def __init__(self, x):
+        self._x = x
+
+    def compute_square(self):
+        return self._x * self._x
