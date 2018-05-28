61b9f8b9bd12722ba70235c21b1d948ea4f8bff0
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-28 08:23:39 +0200

    Example #5 converted into Python 2 and Python 3

---
+++ hy/05_boolean_op_3.py
@@ -0,0 +1,17 @@
+a = 1
+b = 2
+c = 3
+d = 4
+a < b < c
+a > b > c
+1 == 2 == 3 == 4
+True and False
+True or False
+a and b and c and d
+a or b or c or d
+a < 10 or 'nesplneno'
+a < 10 and 'splneno'
+a == b and c == d
+(a or b) and (c or d)
+a + b > 0 and c + d > 0
+
