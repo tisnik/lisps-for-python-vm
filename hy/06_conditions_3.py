7c026ef47035ede5978dab0e1e27ec2396732d37
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-28 08:25:12 +0200

    Example #6 converted into Python 2 and Python 3

---
+++ hy/06_conditions_3.py
@@ -0,0 +1,13 @@
+variableA = 10
+variableB = 20
+variableC = 30
+variableD = 40
+variableE = 50
+variableF = 60
+'mensi' if variableA < variableB else 'vetsi'
+'mensi' if variableA < variableB else None
+84 * (variableC + variableD if variableA == variableB else variableE /
+    variableF)
+'negative' if variableA < 0 else 'positive' if variableA > 0 else 'zero'
+'negative' if variableA < 0 else 'positive' if variableA > 0 else 'zero' if '\ufdd0:else' else None
+
