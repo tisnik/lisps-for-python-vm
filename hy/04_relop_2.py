44951afda62ecb27161a22c47146bdaf7d236862
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-28 08:23:31 +0200

    Example #4 converted into Python 2 and Python 3

---
+++ hy/04_relop_2.py
@@ -0,0 +1,21 @@
+1 < 2
+1 > 2
+1 <= 2
+1 >= 2
+1 == 2
+1 == 1
+1 + 1 == 4 / 2 == 2 ** 10
+variableA = 1
+variableB = 2
+variableC = 3
+variableD = 4
+variableA < variableB
+variableA > variableB
+variableA <= variableB
+variableA >= variableB
+variableA == variableB
+variableA == variableA
+variableA + variableA == variableC / variableD
+variableA == 10 or variableB == 20 or variableC > 0
+variableA > 0 and variableA < 100
+
