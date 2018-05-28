582d025dd686e5b5dc6c9b1da641c8a497b4f86f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-28 16:27:08 +0200

    Hy <-> Python interop

---
+++ hy/14_interop.py
@@ -0,0 +1,18 @@
+import hy
+from interop2 import *
+
+print(calculate_multiplication(6, 7))
+
+print(is_zero(0))
+print(is_zero(1))
+print(is_zero(2))
+
+print(is_even(0))
+print(is_even(1))
+print(is_even(2))
+
+print(is_odd(0))
+print(is_odd(1))
+print(is_odd(2))
+
+print(HIDDEN(1))
---
+++ hy/14_interop2_2.py
@@ -0,0 +1,30 @@
+from hy.core.language import is_even, is_odd, is_zero
+
+
+def calculate_multiplication(x, y):
+    return x * y
+
+
+def is_zero(x):
+    return ==(x, 0)
+
+
+def is_even(x):
+    return is_zero(mod(x, 2))
+
+
+def is_odd(x):
+    return not is_even(x)
+
+
+def string_>bool(s):
+    return ==(s, 'true')
+
+
+def deg_>rad(angle):
+    return angle * (3.1415 / 180)
+
+
+def *HIDDEN*(x):
+    return x + 1
+
