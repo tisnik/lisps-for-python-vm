d99a9e4c3e6130f591a78e7219f23b1a6f3470e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-05-28 08:22:00 +0200

    Example #3 converted into Python 2 and Python 3

---
+++ hy/03_arithmetic_functions_3.py
@@ -0,0 +1,22 @@
+1 - 2
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10
+(1 + 2) * (3 + 4)
+1 * 2 + 3 * 4
+1 / 2
+1 / 2 / 3
+(1 + 2) / (3 + 4)
+10 % 3
+2 ** 10
+0.3 * ((1 + 2) / (3 + 4))
+variableA = 1
+variableB = 2
+variableC = 3
+variableD = 4
+variableA + variableB + variableC + variableD
+(variableA + variableB) * (variableC + variableD)
+variableA * variableB + variableC * variableD
+(variableA + variableB) / (variableC + variableD)
+variableA += 10
+variableB *= 2
+
